a3eafc176d1f87545cc6cd18deb3ad0cfab34921
Abhishek Srivastav <Abhishek.Srivastav@maplelabs.com>
2015-10-15 15:11:06 +0530

    Added yaml_parser to parse yaml file.

---
+++ yaml_parser.py
@@ -0,0 +1,30 @@
+import yaml, traceback
+
+
+class YamlParser():
+    def __init__(self):
+        nfs_server = None
+        nfs_clients = None
+        args = None
+
+    def parse_yaml(self, path):
+        try:
+            with open(path, 'r') as stream:
+                return yaml.load(stream)
+        except IOError:
+            print "Please provide correct file path."
+            print "*"*50
+            traceback.print_exc()
+            
+    def set_properties(self):
+        parsed_yaml = self.parse_yaml('nfs_options.yaml')['NFS_configuration']
+        self.nfs_server = parsed_yaml['nfs-server']
+        self.nfs_clients = parsed_yaml['nfs-clients']
+        self.args = parsed_yaml['args']
+
+
+obj = YamlParser()
+obj.set_properties()
+print obj.nfs_server
+print obj.nfs_clients
+print obj.args
